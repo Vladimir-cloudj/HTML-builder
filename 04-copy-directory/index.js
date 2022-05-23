e9925a7b99a69bad396ecf36bd7d32be82cf7b74
Vladimir <wizart88@gmail.com>
2022-05-24 00:24:47 +0400

    feat: task 04 implement (Tue, May 24, 2022 00:24AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const path = require('path');
+const fsPromises = require('fs').promises;
 
+const dirOriginalPath = path.join(__dirname, 'files');
+const dirCopyPath = path.join(__dirname, 'files-copy');
+
+
+
+async function copyDir()  {
+
+	await fsPromises.mkdir(dirCopyPath, {recursive: true});
+
+	let originalFiles = await fsPromises.readdir(dirOriginalPath);
+
+	for (let file of originalFiles) {
+		await fsPromises.copyFile(path.join(dirOriginalPath, file), path.join(dirCopyPath, file));
+	}
+
+}
+
+
+
+copyDir();
