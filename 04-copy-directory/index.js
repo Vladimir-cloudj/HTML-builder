129d23d98c0ce0952a7804c371985f72ae9f3c97
Vladimir <wizart88@gmail.com>
2022-05-25 15:25:29 +0400

    fix: task 04 - bugs with delete files on 04-copy-directory (Wed, May 25, 2022 15:25PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,7 +7,7 @@ const dirCopyPath = path.join(__dirname, 'files-copy');
 
 
 async function copyDir()  {
-
+  await fsPromises.rm(dirCopyPath, { force: true, recursive: true });
 	await fsPromises.mkdir(dirCopyPath, {recursive: true});
 
 	let originalFiles = await fsPromises.readdir(dirOriginalPath);
@@ -15,9 +15,7 @@ async function copyDir()  {
 	for (let file of originalFiles) {
 		await fsPromises.copyFile(path.join(dirOriginalPath, file), path.join(dirCopyPath, file));
 	}
-
 }
-
 
 
 copyDir();
